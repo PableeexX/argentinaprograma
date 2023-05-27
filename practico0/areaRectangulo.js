561e8dae5175a361e5d14551f7a46327545cad2c
“pablocatcarp@gmail.com” <pablocatcarp@gmail.com>
2023-05-27 12:00:49 -0300

    Practico N0 finalizado

--- practico0/areaRectangulo.js
+++ practico0/areaRectangulo.js
@@ -4,6 +4,7 @@ function areaRectangulo(base,altura){
 console.log("el area de un rectangulo de base 5cm y altura 3cm es " + areaRectangulo(5,3));
 
 console.log("el área de un rectángulo de base 12.5cm y altura 5.3cm es "+areaRectangulo(12.5,5.3));
+ 
 
 
 
--- practico0/promedio3.js
+++ practico0/promedio3.js
@@ -4,3 +4,4 @@ function promedio3 (a,c,b){
 console.log (promedio3(5,7,9));
 console.log (promedio3(-2,-3,-8));
 console.log (promedio3(-1.2,5.3,-3.3));
+ 
