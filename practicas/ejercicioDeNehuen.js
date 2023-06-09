ed6152de3cfaa62f8346dddd6af8b959ab4b9dd5
PableeexX <134019508+PableeexX@users.noreply.github.com>
2023-06-09 18:50:22 -0300

    Delete practicas directory
    elimino carpeta de practicas

--- practicas/ejercicioDeNehuen.js
+++
@@ -1,7 +0,0 @@
-let palabraMixta = "as65d4as65d4as654dsa56";
-// palabraMixta = isNaN (palabraMixta );
-console.log (isNaN (palabraMixta[2]));
-
-
-let numeros = "94564654";
-console.log ( isNaN(numeros));
